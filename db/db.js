8d00ed5d201984c8bf9db2f9f25edbfd0c7e9d7e
Marcos Gonzales <markymarrk@gmail.com>
2020-05-30 18:32:40 -0600

    added port to db configuration

--- db/db.js
+++ db/db.js
@@ -9,7 +9,8 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_URL, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  logging: true
+  logging: true,
+  port: process.env.PORT
 });
 
 // const sequelize = new Sequelize('shoppingCart', 'root', 'fishDogHouse109@!', {
